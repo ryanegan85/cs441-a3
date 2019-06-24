c035cfe100c72a1b459271d66fa81e55b05f749a
ryanegan85 <ryanegan85@gmail.com>
2019-06-24 13:34:09 -0400

    Added functionality to switch betweeen 2 activities through a button press.

---
+++ app/src/main/java/edu/binghamton/cs/cs441_a3/Activity2.java
@@ -0,0 +1,30 @@
+package edu.binghamton.cs.cs441_a3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+    }
+
+    public void openActivity1() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/edu/binghamton/cs/cs441_a3/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.binghamton.cs.cs441_a3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent i = new Intent(this, Activity2.class);
+        startActivity(i);
+    }
+}
