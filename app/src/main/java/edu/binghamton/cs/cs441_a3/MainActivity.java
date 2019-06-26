e41c016f1a597c46153d1364cfda363ab370cf33
ryanegan85 <ryanegan85@gmail.com>
2019-06-25 22:11:40 -0400

    added celsius slider and TextViews to display value in Celcsius and Fahrenheit.

--- app/src/main/java/edu/binghamton/cs/cs441_a3/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/cs441_a3/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private SeekBar seekBar;
+    private TextView seekBarValue;
+    private TextView fValueText;
+    private int fahrenheitValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,34 @@ public class MainActivity extends AppCompatActivity {
                 openActivity2();
             }
         });
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(100);
+
+        seekBarValue = findViewById(R.id.seekBarValue);
+        fValueText = findViewById(R.id.fahrenheitValue);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                fahrenheitValue = (progress * 9/5) + 32;
+                seekBarValue.setText("Celsius: " + progress);
+                fValueText.setText("Fahrenheit: " + fahrenheitValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
-    public void openActivity2() {
+public void openActivity2() {
         Intent i = new Intent(this, Activity2.class);
         startActivity(i);
     }
