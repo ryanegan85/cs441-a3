0dbcd794f46635351fbd1206f0b12f8cde497b5e
ryanegan85 <ryanegan85@gmail.com>
2019-06-28 20:36:57 -0400

    Added descriptors for each picture in activity 2

--- app/src/main/java/edu/binghamton/cs/cs441_a3/Activity2.java
+++ app/src/main/java/edu/binghamton/cs/cs441_a3/Activity2.java
@@ -14,6 +14,7 @@ public class Activity2 extends AppCompatActivity {
     private int celsiusValue;
     private ImageView imageView;
     private TextView textView;
+    private TextView message;
 
 
     @Override
@@ -32,15 +33,37 @@ public class Activity2 extends AppCompatActivity {
         values = getIntent().getStringExtra("TEMP_STRING");
         celsiusValue = Integer.parseInt(getIntent().getStringExtra("TEMP_VALUE"));
         imageView = findViewById(R.id.imageView);
+        message = findViewById(R.id.textView2);
 
         textView.setText(values);
-        if(celsiusValue <= 10) imageView.setImageResource(R.drawable.temp0to10c);
-        if(celsiusValue >= 11 && celsiusValue <= 20) imageView.setImageResource(R.drawable.temp11to20c);
-        if(celsiusValue >= 21 && celsiusValue <= 30) imageView.setImageResource(R.drawable.temp21to30c);
-        if(celsiusValue >= 31 && celsiusValue <= 40) imageView.setImageResource(R.drawable.temp31to40c);
-        if(celsiusValue >= 41 && celsiusValue <= 60) imageView.setImageResource(R.drawable.temp41to60c);
-        if(celsiusValue >= 61 && celsiusValue <= 80) imageView.setImageResource(R.drawable.temp61to80c);
-        if(celsiusValue >= 81) imageView.setImageResource(R.drawable.temp81to100c);
+        if(celsiusValue <= 10) {
+            imageView.setImageResource(R.drawable.temp0to10c);
+            message.setText("Better bring a jacket!");
+        }
+        if(celsiusValue >= 11 && celsiusValue <= 20) {
+            imageView.setImageResource(R.drawable.temp11to20c);
+            message.setText("A cool, crisp fall day.");
+        }
+        if(celsiusValue >= 21 && celsiusValue <= 30) {
+            imageView.setImageResource(R.drawable.temp21to30c);
+            message.setText("April showers bring May flowers.");
+        }
+        if(celsiusValue >= 31 && celsiusValue <= 40) {
+            imageView.setImageResource(R.drawable.temp31to40c);
+            message.setText("A perfectly balmy beach day.");
+        }
+        if(celsiusValue >= 41 && celsiusValue <= 60) {
+            imageView.setImageResource(R.drawable.temp41to60c);
+            message.setText("Hope you brought some water!");
+        }
+        if(celsiusValue >= 61 && celsiusValue <= 80) {
+            imageView.setImageResource(R.drawable.temp61to80c);
+            message.setText("Maybe we should've vacationed somewhere else...");
+        }
+        if(celsiusValue >= 81) {
+            imageView.setImageResource(R.drawable.temp81to100c);
+            message.setText("(Warning: spontaneous combustion may occur)");
+        }
 
     }
 
